b0fb897c96c5fe4abe93ecd75c92c9c64a89be71
Xiaodan Wang <wang.xiaodan@aol.com>
2016-02-16 00:47:44 -0600

    adding collection view and few things in patient views

---
+++ Doctor_Patient/CustCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CustCollectionViewCell.m
+//  Doctor_Patient
+//
+//  Created by Xiaodan Wang on 2/15/16.
+//  Copyright © 2016 钱骏. All rights reserved.
+//
+
+#import "CustCollectionViewCell.h"
+
+@implementation CustCollectionViewCell
+
+@end
---
+++ Doctor_Patient/PatientSelectDocTableViewController.m
@@ -0,0 +1,37 @@
+//
+//  PatientSelectDocTableViewController.m
+//  Doctor_Patient
+//
+//  Created by Xiaodan Wang on 2/16/16.
+//  Copyright © 2016 钱骏. All rights reserved.
+//
+
+#import "PatientSelectDocTableViewController.h"
+
+@interface PatientSelectDocTableViewController ()
+
+@end
+
+@implementation PatientSelectDocTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
